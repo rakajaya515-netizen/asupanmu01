a7dcc226f7c156556eca78469a3011ea5b279d20
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-11 14:27:32 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -4,13 +4,18 @@ export default async function handler(req, res) {
 
   try {
 
-    const page = req.query.page || 1;
+    // ambil query page manual
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    const page =
+      url.searchParams.get("page") || 1;
+
+    console.log("PAGE:", page);
 
     const response = await axios.get(
       `https://vizey.net/api/v1/list?apikey=${process.env.VIZEY_API_KEY}&page=${page}`
     );
 
-    // cache ringan
     res.setHeader(
       "Cache-Control",
       "s-maxage=300, stale-while-revalidate"
