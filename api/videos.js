923020ff97e81053945cd777fbee05e5907b3896
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-11 14:19:23 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
       `https://vizey.net/api/v1/list?apikey=${process.env.VIZEY_API_KEY}&page=${page}`
     );
 
+    // cache ringan
     res.setHeader(
       "Cache-Control",
       "s-maxage=300, stale-while-revalidate"
@@ -18,6 +19,8 @@ export default async function handler(req, res) {
     res.status(200).json(response.data);
 
   } catch (err) {
+
+    console.log(err);
 
     res.status(500).json({
       success: false,
