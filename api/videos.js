f9ee6ecf8f73c575bf9b7e16c9a2b982681e4fed
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-11 16:35:09 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
       // LINK HALAMAN ASLI
       watch:
-        `https://vizey.net/view/${video.id}`
+        `https://vizey.net/v/${video.id}`
 
     }));
 
