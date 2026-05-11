1421377c3fc2c14146908adff671508c3f112dd0
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-11 15:01:54 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -1,65 +1,24 @@
-export default async function handler(req, res) {
+const axios = require('axios');
 
+module.exports = async (req, res) => {
   try {
+    const page = req.query.page || 1;
 
-    const url = new URL(
-      req.url,
-      `http://${req.headers.host}`
-    );
-
-    const page =
-      url.searchParams.get("page") || 1;
-
-    // random anti cache
-    const unique =
-      Date.now() + "_" +
-      Math.floor(Math.random() * 999999);
-
-    const apiUrl =
-      `https://vizey.net/api/v1/list?apikey=${process.env.VIZEY_API_KEY}&page=${page}&nocache=${unique}`;
-
-    console.log(apiUrl);
-
-    const response = await fetch(apiUrl, {
-
-      method: "GET",
-
-      headers: {
-        "Cache-Control": "no-cache",
-        "Pragma": "no-cache"
-      }
-
-    });
-
-    const data = await response.json();
-
-    // disable cache vercel
-    res.setHeader(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    const response = await axios.get(
+      `https://vizey.net/api/v1/list?apikey=${process.env.VIZEY_API_KEY}&page=${page}`
     );
 
     res.setHeader(
-      "Pragma",
-      "no-cache"
+      'Cache-Control',
+      's-maxage=300, stale-while-revalidate=600'
     );
 
-    res.setHeader(
-      "Expires",
-      "0"
-    );
-
-    res.status(200).json(data);
-
-  } catch (err) {
-
-    console.log(err);
-
+    res.status(200).json(response.data);
+  } catch (error) {
     res.status(500).json({
       success: false,
-      error: err.message
+      message: 'Failed fetch video',
+      error: error.message
     });
-
   }
-
-}
+};
